1ed496d283fec0491712c73f0c136f5d867b32e0
Metehan Canpolat <metehancanpolat@outlook.com>
2023-12-20 21:55:36 +0300

    Initial Commit

---
+++ photoMemory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photoMemory
+//
+//  Created by Metehan Canpolat on 20.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
